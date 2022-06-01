14175b6af6ac0af07f75fb3322dd02dff18822b6
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-06-01 19:42:30 -0400

    refactor-get-reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -76,8 +76,31 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+const getAllReservations = async function (guest_id, limit = 10) {
+  try {
+    const { rows } = await pool.query(`SELECT
+    reservations.*,
+    properties.title,
+    properties.cost_per_night,
+    avg(rating) as average_rating
+FROM
+    reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE
+    reservations.guest_id = $1
+GROUP BY
+    properties.id,
+    reservations.id
+ORDER BY
+    reservations.start_date
+LIMIT
+    $2;`, [guest_id, limit]);
+    return rows;
+  } catch (error) {
+    console.log('error')
+    throw(error['message']);
+  }
 }
 exports.getAllReservations = getAllReservations;
 
